2c159e3f86aabeefb10522d7087aca262019ff67
dxjohnson <david.l.johnson@gmail.com>
2015-09-22 23:53:56 -0700

    Fixed to fake_alert_check.sh to use new arguments

--- fake_alert_check.sh
+++ fake_alert_check.sh
@@ -2,7 +2,7 @@
 readonly ALERT_NAME='test_alert'
 readonly MONITOR_DIRECTORY='/tmp/monitors'
 echo "First Check to see if we should alert for all"
-should_alert=$(/usr/bin/python alarmabide/alarmabide.py check $MONITOR_DIRECTORY $ALERT_NAME all)
+should_alert=$(/usr/bin/python alarmabide/alarmabide.py -c check -r all -d $MONITOR_DIRECTORY -a $ALERT_NAME)
 
 if ! $should_alert; then
 	echo "OK for alert $ALERT_NAME"
@@ -12,7 +12,7 @@ fi
 echo "Continues to second check"
 
 for resource in resource1 resource2; do
-	should_alert=$(/usr/bin/python alarmabide/alarmabide.py check $MONITOR_DIRECTORY $ALERT_NAME $resource)
+	should_alert=$(/usr/bin/python alarmabide/alarmabide.py -c check -d $MONITOR_DIRECTORY -a $ALERT_NAME -r $resource)
 
 	if ! $should_alert; then
 		echo "OK for resource: $resource"
